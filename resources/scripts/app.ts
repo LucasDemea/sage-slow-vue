6007053e70b20190bd2f782400a07b21ebcf7a03
Kelly Mears <developers@tinypixel.dev>
2023-04-30 14:16:19 -0400

    ✨ improve: dom should be available on load

--- resources/scripts/app.ts
+++ resources/scripts/app.ts
@@ -1,17 +1,12 @@
-import domReady from '@roots/sage/client/dom-ready';
 import { createApp } from 'vue';
 import { NMenu } from 'naive-ui';
-/**
- * Application entrypointé
- */
-domReady(async () => {
-  const nav = createApp({
-    components: {
-      'n-menu': NMenu,
-    },
-    setup() {
-      console.log('teuds');
-      const menuItems = [];
-    },
-  });
+
+createApp({
+  components: {
+    'n-menu': NMenu,
+  },
+  setup() {
+    console.log('teuds');
+    const menuItems = [];
+  },
 });
